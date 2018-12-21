52aebf399b613ef0bfbeda2385f02301729ef502
Alex Barbul <barbul-s@ukr.net>
2018-12-21 17:25:41 +0200

    update

--- worker/ec2-web.tf
+++ worker/ec2-web.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "amber-web" {
 
-  ami             = "${var.ami-linux}"
+  ami             = "ami-0080e4c5bc078760e"
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ambersg.name}"]
   key_name        = "axs"
