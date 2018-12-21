dbf9ee1873b0dab50e4630f292e613c462fc4be6
Alex Barbul <barbul-s@ukr.net>
2018-12-21 17:49:15 +0200

    update SG

--- worker/sg-web.tf
+++ worker/sg-web.tf
@@ -12,21 +12,21 @@ resource "aws_security_group" "ambersg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["194.105.145.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["194.105.145.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 21
     to_port     = 21
     protocol    = "tcp"
-    cidr_blocks = ["194.105.145.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
