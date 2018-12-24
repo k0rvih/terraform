575861567bb74ede9bfd3a657a7d89e302a425ed
Alex Barbul <barbul-s@ukr.net>
2018-12-24 17:49:51 +0200

    add aws acc to backend

--- worker/backend.tf
+++ worker/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket = "ambertf"
     key    = "amber/terraform.tfstate"
     region = "us-east-1"
-    access_key = "AKIAICCR74Q363JHODJQ"
-    secret_key = "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"
+    profile = "${var.aws_profile}"
   }
 }
--- worker/variables.tf
+++ worker/variables.tf
@@ -3,3 +3,6 @@ variable "main_ip" {
 
 variable "ami_linux" {
 }
+
+variable "aws_profile" {
+}
