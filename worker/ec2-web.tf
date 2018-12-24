807e414e4188fa6226d388e877e6c0ce3b4745a0
Alex Barbul <barbul-s@ukr.net>
2018-12-24 17:21:54 +0200

    update

--- worker/ec2-web.tf
+++ worker/ec2-web.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "amber-web" {
 
-  ami             = "${var.ami-linux}"
+  ami             = "${var.ami_linux}"
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ambersg.name}"]
   key_name        = "axs"
--- worker/terraform.tfvars
+++ worker/terraform.tfvars
@@ -1,2 +1,2 @@
 main_ip = "194.105.145.0"
-ami-linux = "ami-0080e4c5bc078760e"
+ami_linux = "ami-0080e4c5bc078760e"
