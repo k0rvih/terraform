bb74c0ef70c8295c18b23d1a3c94851f90270632
Alex Barbul <barbul-s@ukr.net>
2018-12-26 13:51:10 +0200

    update

--- worker/sg-web.tf
+++ worker/sg-web.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "ambersg" {
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "All"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
