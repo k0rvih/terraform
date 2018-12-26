dd2547d556517cefd92f08a4762cec2260654a24
Alex Barbul <barbul-s@ukr.net>
2018-12-26 13:58:10 +0200

    update

--- worker/ec2-web.tf
+++ worker/ec2-web.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "amber-web" {
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ambersg.name}"]
   key_name        = "axs"
+  #user_data = "${data.template_file.ec2_user_data.rendered}"
   
   tags {
     Name = "Octopus Worker"
