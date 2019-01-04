1083b3b4b3908d9d5a6b1107067c19e33d405105
Alex Barbul <barbul-s@ukr.net>
2019-01-04 11:47:21 +0200

    update variables

--- worker-win/ec2-web.tf
+++ worker-win/ec2-web.tf
@@ -11,12 +11,13 @@ resource "aws_instance" "amber-web" {
   $wc1 = New-Object SYSTEM.Net.WebClient
   $octoapi = "${var.octo_api}"
   $octoserver = "${var.octo_server}"
+  $role = "${var.octorole}"
+  $env = "${var.octoenv}"
   $ipAddress = $wc1.Downloadstring('http://169.254.169.254/latest/meta-data/public-hostname')
   New-Item $output -ItemType Directory
   cd $output
   Invoke-WebRequest -Uri $url -OutFile $output\octo.msi
-  msiexec /i octo.msi /quiet
-  Start-Sleep -s 20
+  Start-Process octo.msi -Wait -ArgumentList "/i","/quiet"
   cd "C:\Program Files\Octopus Deploy\Tentacle"
   .\Tentacle.exe create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle.config" --console
   .\Tentacle.exe new-certificate --instance "Tentacle" --if-blank --console
@@ -24,7 +25,7 @@ resource "aws_instance" "amber-web" {
   .\Tentacle.exe configure --instance "Tentacle" --home "C:\Octopus" --app "C:\Octopus\Applications" --port "10933" --console
   .\Tentacle.exe configure --instance "Tentacle" --trust "142B8DC3922A94F8407EBAF54749263FC8BB2C4C" --console
   netsh advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=10933
-  .\Tentacle.exe register-with --instance "Tentacle" --publicHostName=$ipAddress --server $octoserver --apiKey=$octoapi --role "web" --environment "Dev" --comms-style TentaclePassive --console
+  .\Tentacle.exe register-with --instance "Tentacle" --publicHostName=$ipAddress --server $octoserver --apiKey=$octoapi --role $role --environment $env --comms-style TentaclePassive --console
   .\Tentacle.exe service --instance "Tentacle" --install --start --console
   </powershell>
   EOF
--- worker-win/variables.tf
+++ worker-win/variables.tf
@@ -2,3 +2,6 @@ variable "ami_linux" {}
 variable "octo_api" {}
 variable "octo_url" {}
 variable "octo_server" {}
+variable "octorole" {}
+variable "octoenv" {}
+
