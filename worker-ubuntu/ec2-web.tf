8b872a1583edee7098a3cfa8a893915e645029ac
Alex Barbul <barbul-s@ukr.net>
2019-01-07 19:44:27 +0200

    add script to user data

--- worker-ubuntu/ec2-web.tf
+++ worker-ubuntu/ec2-web.tf
@@ -5,13 +5,15 @@ resource "aws_instance" "amber-web" {
   security_groups = ["${aws_security_group.ambersg.name}"]
   key_name        = "axs"
   user_data = <<EOF
+  <script>
   #!/bin/bash
   url="${var.terraform_linuxurl}"
   trpackage="${var.trpackage}"
   sudo apt install unzip
-  wget $url
-  unzip $trpackage
+  wget $$url
+  unzip $$trpackage
   sudo mv terraform /usr/local/bin/
+  </script>
   EOF
   
   tags {
