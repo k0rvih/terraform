200b92ed8625083463824bda08138ee1751068cc
k0rvih <barbul-s@ukr.net>
2019-01-08 15:24:10 +0200

    Update backend.tf

--- worker-ubuntu/backend.tf
+++ worker-ubuntu/backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket = "ambertf"
     key    = "amberubuntu/terraform.tfstate"
     region = "us-east-1"
-    access_key = "AKIAIZF7EDRRV36E4NWQ"
-    secret_key = "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"
   }
 }
