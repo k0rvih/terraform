b9ae3006351c24d3cd88ee72e01403882b42ae4c
Alex Barbul <barbul-s@ukr.net>
2019-01-08 14:13:34 +0200

    update

--- worker-ubuntu/ec2-web.tf
+++ worker-ubuntu/ec2-web.tf
@@ -5,20 +5,16 @@ resource "aws_instance" "amber-web" {
   security_groups = ["${aws_security_group.ambersg.name}"]
   key_name        = "axs"
   user_data = <<EOF
-  #!/bin/bash
-  url="https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip"
-  trpackage="terraform_0.11.11_linux_amd64.zip"
-  sudo apt install unzip
-  wget $url
-  unzip $trpackage
-  sudo mv terraform /usr/local/bin/
-  EOF
+              #!/bin/bash
+              url="${var.terraform_linuxurl}"
+              trpackage="${var.trpackage}"
+              yum update -y
+              yum install unzip
+              wget $url
+              unzip $trpackage
+              mv terraform /usr/local/bin/
+              EOF
   
   tags {
     Name = "Octopus Worker Ubuntu"
-    Schedule = "kyiv-office-hours"
   }
-
-}
-
-
