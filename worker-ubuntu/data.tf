7abcb698faec7f2dc4a3603991bf373aeb070584
Alex Barbul <barbul-s@ukr.net>
2019-01-09 15:42:29 +0200

    new aws account

--- worker-ubuntu/data.tf
+++ worker-ubuntu/data.tf
@@ -1,3 +1,8 @@
 data "template_file" "worker" {
     template = "${file("user_data.sh")}"
+
+    vars {
+        trpackage = "${var.trpackage}"
+        url = "${var.terraform_linuxurl}"
+    }
 }
--- worker-ubuntu/user_data.sh
+++ worker-ubuntu/user_data.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 su -c 'curl https://download.mono-project.com/repo/centos7-stable.repo | tee /etc/yum.repos.d/mono-centos7-stable.repo'
-url="${var.terraform_linuxurl}"
-trpackage="${var.trpackage}"
 yum install mono-devel -y
 yum update -y
 yum install unzip
