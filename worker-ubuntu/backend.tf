e218d4ca768b9762072d065821ebed87db08f7a9
Alex Barbul <barbul-s@ukr.net>
2019-01-09 15:25:56 +0200

    new aws account

--- worker-ubuntu/backend.tf
+++ worker-ubuntu/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ambertf"
+    bucket = "ambertr"
     key    = "amberubuntu/terraform.tfstate"
     region = "us-east-1"
-    access_key = "AKIAIZF7EDRRV36E4NWQ"
-    secret_key = "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"
   }
 }
--- worker-ubuntu/ec2-web.tf
+++ worker-ubuntu/ec2-web.tf
@@ -4,19 +4,7 @@ resource "aws_instance" "amber-web" {
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ambersg.name}"]
   key_name        = "axs"
-  user_data = <<EOF
-              #!/bin/bash
-              rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-              su -c 'curl https://download.mono-project.com/repo/centos7-stable.repo | tee /etc/yum.repos.d/mono-centos7-stable.repo'
-              url="${var.terraform_linuxurl}"
-              trpackage="${var.trpackage}"
-              yum install mono-devel -y
-              yum update -y
-              yum install unzip
-              wget $url
-              unzip $trpackage
-              mv terraform /usr/local/bin/
-              EOF
+  user_data = "${data.template_file.worker.rendered}"
 
     tags {
     Name = "Octopus Worker Ubuntu"
