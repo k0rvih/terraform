2e8a8bb670eb8b0c29cb0f49c41ca25e17027940
Alex Barbul <barbul-s@ukr.net>
2019-01-10 15:41:54 +0200

    change user data

--- worker-win/user_data.ps1
+++ worker-win/user_data.ps1
@@ -3,7 +3,7 @@ $publicHostName = (Invoke-WebRequest -Uri "http://169.254.169.254/latest/meta-da
 $output = "C:\install"
 New-Item $output -ItemType Directory
 Set-Location $output
-Invoke-WebRequest -Uri $url -OutFile $output\octo.msi
+Invoke-WebRequest -Uri ${url} -OutFile $output\octo.msi
 Start-Process "octo.msi" -Wait -ArgumentList "/quiet"
 Set-Location "C:\Program Files\Octopus Deploy\Tentacle"
 .\Tentacle.exe create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle.config" --console
@@ -12,6 +12,6 @@ Set-Location "C:\Program Files\Octopus Deploy\Tentacle"
 .\Tentacle.exe configure --instance "Tentacle" --home "C:\Octopus" --app "C:\Octopus\Applications" --port "10933" --console
 .\Tentacle.exe configure --instance "Tentacle" --trust "142B8DC3922A94F8407EBAF54749263FC8BB2C4C" --console
 netsh advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=10933
-.\Tentacle.exe register-with --instance "Tentacle" --publicHostName=$publicHostName --server $octoserver --apiKey=$octoapi --role $role --environment $env --comms-style TentaclePassive --console
+.\Tentacle.exe register-with --instance "Tentacle" --publicHostName=$publicHostName --server ${octoserver} --apiKey=${octoapi} --role ${role} --environment ${env} --comms-style TentaclePassive --console
 .\Tentacle.exe service --instance "Tentacle" --install --start --console
 </powershell>
